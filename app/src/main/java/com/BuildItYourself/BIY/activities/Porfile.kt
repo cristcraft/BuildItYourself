1ad2cfc94e7d33c0c598beb033cbbfab870d7db4
cristcraft <cristmateus29@gmail.com>
2025-11-06 12:57:40 +1100

    Carga de informacion inicial

---
+++ app/src/main/java/com/BuildItYourself/BIY/activities/Porfile.kt
@@ -0,0 +1,21 @@
+package com.BuildItYourself.BIY.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.BuildItYourself.BIY.R
+
+class Porfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_porfile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
---
+++ app/src/main/java/com/BuildItYourself/BIY/activities/Splash.kt
@@ -0,0 +1,28 @@
+package com.BuildItYourself.BIY.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.BuildItYourself.BIY.MainActivity
+import com.BuildItYourself.BIY.R
+
+class Splash : AppCompatActivity() {
+    private val splash_duration = 3000L
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            //logica del redireccionamiento
+            val intent = Intent(this, Inicio::class.java)
+            startActivity(intent)
+            finish()
+        }, splash_duration)
+    }
+}
